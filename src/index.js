8083ed0502c2d3aa8e748aa588b00c4fa2fd9a16
Melissa Rice <melissa.rice@perficient.com>
2021-08-10 10:57:45 -0400

    got rid of constructor

--- src/index.js
+++ src/index.js
@@ -2,24 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: "" };
 
-    // this is the only time we do direct assignment to this.state
-    // to change state, you always call setState
-    this.state = { lat: null, errorMessage: "" };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       // success callback
-      (position) => {
-        // we called setState to update our state
-        this.setState({ lat: position.coords.latitude });
-      },
+      (position) => this.setState({ lat: position.coords.latitude }),
       // error callback
       (err) => this.setState({ errorMessage: err.message })
     );
   }
-
 
   // we have to define render
   render() {
